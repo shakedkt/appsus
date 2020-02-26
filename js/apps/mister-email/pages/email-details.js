70c1e6dbc90199d260c4431547c175a1f9009461
shaked katsir <shakedkat@gmail.com>
2020-02-26 17:29:19 +0200

    new

---
+++ js/apps/mister-email/pages/email-details.js
@@ -0,0 +1,41 @@
+import {emailService} from '../services/email-service.js'
+
+export default {
+    template:`
+        <section class="car-details-container" v-if="car">
+            <h1>CAR DETAILS - {{car.vendor}}</h1>
+            <pre>{{car}}</pre>
+           
+            <section v-if="nextPrevCarIds">
+                <router-link :to="nextPrevCarIds.prevId">PREV</router-link>
+                <router-link :to="nextPrevCarIds.nextId">NEXT</router-link>
+            </section>
+        </section>
+    `,
+    data(){
+        return {
+            car:null,
+            nextPrevCarIds : null
+        }
+    },
+   
+    watch: {
+        '$route.params.id'(to,from){
+            console.log('ROUTE CHANGED, loading a car');
+            this.getCar()
+        }
+    },
+    methods:{
+        getCar(){
+            const carId = this.$route.params.id
+            emailService.getById(carId)
+            .then(car => {
+                this.car = car 
+                this.nextPrevCarIds = emailService.getNextPrevCarIds(car.id);   
+            })
+        }
+    },
+    created(){
+        this.getCar()
+    }
+}
