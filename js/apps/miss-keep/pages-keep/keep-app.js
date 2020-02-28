44d604ff49016ef453d17ca6744636cfee4a653b
Rotem Yehiel <rotem8y@gmail.com>
2020-02-28 10:15:38 +0200

    today

--- js/apps/miss-keep/pages-keep/keep-app.js
+++ js/apps/miss-keep/pages-keep/keep-app.js
@@ -10,18 +10,6 @@ export default {
     template: `
     <section class="miss-keep-container">
         <h1>Miss Keep</h1>
-<<<<<<< HEAD
-        <h2 v-if="notes">notes</h2>
-        <ul>
-            <li v-for="(note, idx) in notes" >
-                <!-- if its componenet of this type (text) so we render it like this -->
-                <h4>{{note.type}}, {{note.isPinned}},{{note.info.txt}}</h4>  
-            </li>
-        </ul>
-        <!-- <email-filter @set-filter="setFilter"></email-filter>
-        <button class="compose-btn" @click="composeNewMail">Compose</button>
-        <email-list v-if="emails" :emails="emailsForDisplay"></email-list> -->
-=======
         <h2>notes</h2> 
         <div class="make-note">
             <label>Make New Note</label>
@@ -39,7 +27,6 @@ export default {
           <!-- <componenet :is="note.type" :notes="notesForDisplay" v-for="(note, idx) in notes"></componenet> -->
       
      
->>>>>>> 387fe4e7475c96a77cabfd933700771203edbf78
     </section>
     `,
     data() {
